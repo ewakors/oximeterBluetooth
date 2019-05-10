7d35c357da8af4aec693e6446e3bc6f7ee8d825f
Ewa Korszaczuk <e.korszaczuk@goapps.pl>
2019-05-10 15:23:56 +0200

    Initial Commit

---
+++ TestBluetooth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestBluetooth
+//
+//  Created by goapps on 10/05/2019.
+//  Copyright © 2019 pl.goapps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
